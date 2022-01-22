72e59851ff3d253f57b108380874163648b39aa9
fabiloco <faalsaru@gmail.com>
2022-01-22 14:38:31 -0500

    date pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     nombre: string = 'Capitan America';
+    arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    PI: number = Math.PI;
+    porcentaje: number = 0.234;
+    salario: number = 12345;
+    fecha: Date = new Date();
+
+    idioma : string = 'fr';
+
+    valorPromesa = new Promise<string>((resolve) => {
+        setTimeout(() => { resolve('Llego la data') }, 4500)
+    });
+
+    heroe = {
+        nombre: 'Logan',
+        clave: 'Wolverine',
+        edad: 500,
+        direccion: {
+            calle: 'Primera',
+            casa: 20,
+        },
+    };
+
+    changeLanguage(idioma: string) {
+        this.idioma = idioma;
+    };
 };
